ac0e5980656b54d20a5e3ea5e18f478d609cf919
ashleeparadis <ashlee.paradis@outlook.com>
2025-02-24 00:55:32 +0000

    Steps 3.1 to 3.3 completed

---
+++ homework2/movie_theater_booking/bookings/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#Movie class to define title, description, release date, and length of movie
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+#Seat class to define the seat number and whether or not the seat is available to buy
+class Seat(models.Model):
+    seat_num = models.CharField(max_length=30, unique=True)
+    is_booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Seat {self.seat_num}: {'Booked' if self.is_booked else 'Available'}"
+
+class Booking(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    seat = models.OneToOneField(Seat, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title} - Seat {self.seat.seat_number}"
---
+++ homework2/movie_theater_booking/bookings/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie, Seat, Booking
+from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
